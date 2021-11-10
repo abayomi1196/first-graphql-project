c84f9ee55d86fca18ad249295a45485fb7983a0c
Abayomi Ayodele <samabayomi301@gmail.com>
2021-11-10 11:46:27 +0100

    setup apollo client

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// apollo
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// setup client
+const client = new ApolloClient({
+  url: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* wrap application with Provider with created client */}
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
